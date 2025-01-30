9ebada2e3133c268c803a937d6c6090f4fe9fca3
unknown <harshika0102@gmail.com>
2025-01-30 10:26:49 +0530

    add ss

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Header from './components/header'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route } from 'react-router-dom';  // Import Routes and Route
+import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
 
 function App() {
- return (
-
-<Header/> 
-
-
-
-
- );
+  return (
+    <Router>  {/* Wrap the entire app inside Router */}
+      <Header />
+      <Routes>
+        {/* Define your routes here */}
+        {/* Example Route */}
+        <Route path="/" element={<h1>Home Page</h1>} />
+        {/* You can add more routes as needed */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/header.js
+++ src/components/header.js
@@ -1,29 +1,30 @@
 import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import { NavLink } from 'react-router-dom';
-import { NavItem } from 'react-bootstrap';  // Correct import if you're using react-bootstrap
+import { Navbar, Nav, NavItem } from 'react-bootstrap';  // Importing components from react-bootstrap
+import { NavLink } from 'react-router-dom';  // Import NavLink from react-router-dom for routing
 
-const Header = () => {  // Change header to Header (capitalized)
+const Header = () => {
   return (
-    <>
-      <Nav pills>
-        <NavItem>
-          <NavLink active href="#">Link</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="#">Link</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="#">Another Link</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink disabled href="#">Disabled Link</NavLink>
-        </NavItem>
-      </Nav>
-    </>
+    <Navbar bg="light" expand="lg">  {/* Navbar with light background and responsive expand on large screens */}
+      <Navbar.Brand href="/">Brand</Navbar.Brand>  {/* Your brand/logo here */}
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />  {/* Toggle button for mobile */}
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">  {/* Aligning the navigation items to the right */}
+          <NavItem>
+            <NavLink exact to="/" className="nav-link" activeClassName="active">Home</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/about" className="nav-link" activeClassName="active">About Us</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/services" className="nav-link" activeClassName="active">Services</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/contact" className="nav-link" activeClassName="active">Contact Us</NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
-export default Header;  // Ensure the export is using the correct case
+export default Header;
